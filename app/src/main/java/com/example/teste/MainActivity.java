ed77abb75cba7ffcff4674ed274590f5a38e7252
João <ContactorGomes@protonmail.ch>
2023-03-14 00:54:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/teste/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.teste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Object[] Sexo = {"Feminino", "Masculino"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void buttonCalcular() {
+
+    }
+
+
+
+}

53238d6f5b08495b6f81d9a0ef28e7faf719b122
João <ContactorGomes@protonmail.ch>
2023-03-20 13:22:39 -0300

    Floating Action Button, questionario de perguntas e respostas

--- app/src/main/java/com/example/teste/MainActivity.java
+++ app/src/main/java/com/example/teste/MainActivity.java
@@ -3,22 +3,29 @@ package com.example.teste;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Object[] Sexo = {"Feminino", "Masculino"};
+    RadioGroup groupProfessores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        groupProfessores = (RadioGroup)  findViewById(R.id.idGroup);
     }
 
 
-    public void buttonCalcular() {
-
+    public void floatButtonClick(View view) {
+        RadioButton radioSelecionado = (RadioButton)  findViewById(groupProfessores.getCheckedRadioButtonId());
+        if (radioSelecionado.getText().toString().equals("Rafael Moreno"))
+            Toast.makeText(this, "Resposta Correta!", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(this, "Resposta Errada, quem leciona essa matéria é o Rafael Moreno", Toast.LENGTH_SHORT).show();
     }
-
-
 
 }
